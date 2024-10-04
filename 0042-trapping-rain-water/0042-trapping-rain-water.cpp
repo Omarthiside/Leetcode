7e4e8a9cbb422e09d037b0d053f6f2e57c0d50aa
Omar Abdulla <129420152+Omarthiside@users.noreply.github.com>
2024-10-05 00:46:51 +0530

    Time: 13 ms (53.27%), Space: 25.7 MB (35.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,35 +1,29 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
+
 
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if (n == 0) return 0;
+        if (n == 0) return 0;  
 
-        int left = 0, right = n - 1;
-        int left_max = 0, right_max = 0;
-        int w = 0; // total water trapped
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= left_max) {
-                    left_max = height[left]; 
-                } else {
-                    w += left_max - height[left]; 
-                }
-                left++;
-            } else {
-                if (height[right] >= right_max) {
-                    right_max = height[right]; 
-                } else {
-                    w += right_max - height[right]; 
-                }
-                right--;
-            }
+        vector<int> leftMax(n), rightMax(n);
+        int w = 0;
+        
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
         }
 
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int wi = min(leftMax[i], rightMax[i]) - height[i];
+            w += wi;
+        }
+        
         return w;
     }
 };
