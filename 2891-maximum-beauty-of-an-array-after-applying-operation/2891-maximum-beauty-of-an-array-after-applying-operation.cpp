aa971a2f1dd080c4c7d0e6199cb905c7978ab2fc
Omar Abdulla <129420152+Omarthiside@users.noreply.github.com>
2024-12-11 18:01:56 +0530

    Time: 50 ms (63.14%), Space: 105 MB (67.7%) - LeetHub

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int maximumBeauty(vector<int>& nums, int k) {
+    int l = 0;
+    int r = 0;
+
+    ranges::sort(nums);
+
+    for (r = 0; r < nums.size(); ++r)
+      if (nums[r] - nums[l] > 2 * k)
+        ++l;
+
+    return r - l;
+  }
+};
